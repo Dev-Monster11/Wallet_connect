90ca3b858a51c902665620b9970b38a2e6c20cae
DevMonster <95547649+EnterKaka@users.noreply.github.com>
2022-04-08 22:22:46 +0200

    update app.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './screens/Home';
+import Mobile from "./screens/Mobile"
+import {isMobile} from 'react-device-detect';
+import Fetch from "./screens/fetch"
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+     {/* {isMobile == true ? <Mobile />  : <Home />} */}
+     <Home />
+     {/* <Fetch /> */}
+    {/* kk */}
+    </div>
+  );
+}
+
+export default App;
